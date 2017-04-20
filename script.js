70998edb65224026bdb878b06dfd2270c5c299c0
soprasteria <soprasteria>
2017-04-20 06:26:09 +0200

    Caricamento dei restanti file

---
+++ script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+  $('#B1').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Chees Cake Frutti di Bosco</h2><p>Dosi & Ingredienti</p><h4>DIFFICOLTÀ</h4><p>bassa</p><h4>PREPARAZIONE</h4><p>25 min</p><h4>COTTURA</h4><p>50 min</p><h4>COSTO</h4><p>basso</p><h4>DOSI PER 8 persone:</h4><p>250 g di oro saiwa</p><p>150 g di burro</p><p>4 uova</p><p>4 cucchiai di zucchero</p><p>200 ml di panna da cucina</p><p>200 ml di panna da montare</p><p>250 g di ricotta</p><p>1 bustina di vanillina</p><p>250 g di marmellata di frutti di bosco.</p></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B2').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Torta mimosa all'ananas</h2><p>L’8 marzo è un giorno speciale, si omaggiano le donne con un fiore che ha il colore del sole e che profuma di buono… oppure con un dolce che conserva le stesse caratteristiche: la torta mimosa! Il nome di questo dolce deriva proprio dal fatto che questa torta viene decorata con del Pan di Spagna sbriciolato: ricorda appunto i grappoli di questo splendido fiore primaverile. Per la torta mimosa è necessario preparare due Pan di Spagna, uno sarà farcito con una golosa crema pasticcera arricchita da panna montata: otterrete una soffice crema diplomatica. L’altro Pan di Spagna occorrerà per ricavare la copertura della base: piccoli cubetti che serviranno a ricoprire la torta per creare l’effetto “mimosa”. Preparate questa torta per una serata tra amiche, per le donne della famiglia o per gustare quando più preferirete questo dolce intramontabile!</p><h4>Ingredienti per 2 Pan di Spagna del diametro di 20 cm.</h4><p>Zucchero 250 g</p><p>Farina 00 140 g </p><p>Fecola di patate 120 g </p><p>Uova 8 </p><p>Baccello di vaniglia </p><p>2 Sale fino 1 pizzico</p><h4>PER 850 G DI CREMA PASTICCERA</h4><p>Tuorli 5 </p><p>Zucchero 175 g </p><p>Latte intero 500 ml </p><p>Panna fresca liquida 125 ml </p><p>Amido di mais (maizena) 55 g </p><p>Baccello di vaniglia 1</p></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B3').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Nutellotti</h2><p>preparazione: 10 min cottura: 8 min totale: 18 min</p><h4>Ingredienti per 20 biscotti:</h4><ul><li>180 gr di nutella</li><li>150 gr di farina 00</li><li>1 uovo</li><li>zucchero a velo</li><li>150 gr di nutella per la farcitura</li></ul></div></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B4').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Crostata di fragole</h2><p>La crostata di fragole fresche è uno dei dolci più classici ed invitanti che si possa realizzare con questi rossi e profumati frutti primaverili. La crostata di fragole è preparata con una base di pasta frolla aromatizzata al limone, farcita con golosa crema pasticcera, sulla quale verranno adagiate le fragole fresche; potrete creare un effetto molto elegante disponendo le fragole a raggiera su tutta la superficie ed ottenere un effetto lucido con una gelatina leggera da realizzare in casa. Sorprendete i vostri amici con la crostata di fragole per un fine pasto o una merenda dolce e fresca.</p><h4>Ingredienti per la pasta frolla (per una tortiera di 28 cm di diametro)</h4><ul><li>Farina 00 250 g</li><li>Burro freddo di frigo 125 g</li><li>Zucchero a velo 100 g</li><li>Tuorli 2</li><li>Scorza di limone non trattato 1</li></ul><h4>PER LA CREMA PASTICCERA</h4><ul><li>Tuorli 6</li><li>Amido di mais (maizena) 45 g</li><li>Zucchero 140 g</li><li>Latte intero fresco 400 ml</li><li>Panna fresca liquida 100 ml</li><li>Baccello di vaniglia 1</li></ul><h4>PER LO SCIROPPO</h4><ul><li>Zucchero 100 g</li><li>Acqua 100 ml</li><li>Succo di mezzo limone</li></ul><h4>PER DECORARE</h4><ul><li>Fragole 600 g</li></ul></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B5').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Tiramisù</h2><p>Anticamente le nonne usavano preparare una colazione che oggi potremmo definire povera, ma che all’epoca era davvero ricchissima! Ancora oggi la ricetta è immutata, basta rompere in un bicchiere un uovo freschissimo e sbatterlo con dello zucchero e a piacere aggiungere caffè o latte caldo, per i più piccoli, e marsala o anice per i più grandicelli. Ed è proprio da questa portentosa crema che nasce la crema al mascarpone base del tiramisù. Il dolce italiano per eccellenza, quello più famoso e amato, ma soprattutto che ha dato vita a tantissime altre versioni! Le più apprezzate? Sicuramente il tiramisù alle fragole o quello alla Nutella, giusto per citarne qualcuno! Sebbene le origini di questo famoso dessert non siano chiare, perché contese tra le regioni del Veneto, Piemonte e Toscana, resta comunque un caposaldo della cucina italiana preparato indistintamente da Nord a Sud. Purtroppo non abbiamo scoperto chi l’ha inventato ma sicuramente sappiamo come farvi preparare uno dei più buoni tiramisù che abbiate mai preparato. Perciò ecco la ricetta: preparatelo anche voi e fateci sapere!</p><h4>Ingredienti</h4><ul><li>Savoiardi 300 g</li><li>Uova freschissime (circa 4 medie) 220 g</li><li>Mascarpone freddo 500 g</li><li>Zucchero 100 g</li><li>Caffè della moka già pronto (e zuccherato a piacere) 300 g</li><li>Cacao amaro in polvere per la superficie q.b.</li></ul></div></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B6').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Torta Pan di Stelle</h2><p>La torta Pan di stelle è un dolce supergoloso composto da un'alternanza di strati di biscotti Pan di stelle imbevuti nel latte, nutella e panna montata,una cosa leggera insomma ;)</p><p>Preparazione: 20 min cottura:  totale: 20 min + 2 ore in frigo</p><h4>Ingredienti per uno stampo da 22cm:</h4><ul><li>500 gr di biscotti pan di stelle</li><li>400 gr di nutella</li><li>500 ml di panna fresca</li><li>50 gr di zucchero a velo</li><li>150 ml di latte</li><li>1 tazzina di caffè</li><li>cacao amaro</li></ul></div></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B7').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Cupcake alla Vaniglia</h2><p>I cupcake sono una specie di muffin farciti. I cupcake sono grandi come una tazza da tè ed è da questo che prendono il loro nome. Più che per la bontà e per morbidezza, i cupcakes sono particolari perché si prestano a mille decorazioni, ed è proprio la decorazione il loro punto forte! Questa è una ricetta base per fare i cupcake alla vaniglia, però voi potete sbizzarrirvi sia con l'impasto che con le decorazioni.<p>Preparazione: 25 min cottura: 15 min totale: 40 min</p><h4>Ingredienti per 10 cupcake:</h4><p>2 uova</p> <p>100 gr di burro</p><p>110 gr di zucchero</p><p>140 gr di farina 00</p><p>30 ml di latte</p><p>2 cucchiaini di lievito per dolci</p><p>1 cucchiaino di essenza di vaniglia</p></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#B8').on('click',function(){
+    $('div#caricaRicetta').html("<div class=\"col-lg-12 col-md-12 col-sm-12\"><h2>Torta ai biscotti con Crema Chantilly e nutella</h2><p>Una torta casalinga, una mattonella composta da strati di biscotti secchi alternati alla crema pasticcera. Una ricetta che in ogni famiglia veniva realizzata in modo diverso, ma sempre facile e a base di pochi, gustosi, ingredienti. La torta con biscotti secchi e crema pasticcera è un dolce che dopo un giorno in frigorifero è ancora più buono!</p><h4>INGREDIENTI</h4><p>• 40 biscotti secchi</p><p>• crema pasticcera e nutella</p><p>• 50 g cacao in polvere</p><p>• cioccolato in scaglie</p><p>• latte</p><p>• caffè</p><p>Una ricetta che a molti ricorderà l'infanzia quella della torta con biscotti secchi e crema pasticcera, anche se in ogni famiglia veniva realizzata con qualche piccolo particolare differente.</p></div>");
+    $('#stelle').html("<h4>Vota la ricetta</h4>");
+    $('#stelle2').raty({
+        half:  true,
+        starOn:"bower_components/raty/lib/images/star-on.png",
+        starOff:"bower_components/raty/lib/images/star-off.png",
+        starHalf:"bower_components/raty/lib/images/star-half.png"
+      });
+  });
+  $('#myCarousel>a').on('click',function(){
+    $('div#caricaRicetta').html("");
+    $('div#stelle').html("");
+    $('div#stelle2').html("");
+  });
+});
